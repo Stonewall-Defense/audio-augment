1c5a61c9fc1c7d8cb668b12c06650e2c9827b143
Ryan Quinn <ryan.quinn@certusinnovations.com>
2025-10-28 13:57:49 -0400

    feat: Installable

---
+++ audio-augment/__init__.py
@@ -0,0 +1,3 @@
+from EscBackgroundNoise import create_esc_augment
+from Infrasound import Infrasound, NoiseLevelType
+from MadBackgroundNoise import make_mad_bg_exclude, make_mad_bg_multi_class, make_mad_bg_single_class, MadClass
