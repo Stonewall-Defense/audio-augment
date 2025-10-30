ca63c04242d64d858104073e0819a7c73acec52c
Ryan Quinn <ryan.quinn@certusinnovations.com>
2025-10-30 13:56:53 -0400

    fix: LRU cache size, max_samples, and MAD class selection

--- audioaug/MadBackgroundNoise.py
+++ audioaug/MadBackgroundNoise.py
@@ -32,13 +32,15 @@ def _make_mad_bg_common(mad_files: list[str],
                         min_snr_db: float = 3.0,
                         max_snr_db: float = 30.0,
                         p: float = 0.5,
-                        max_samples: int = 1_000,
+                        max_samples: int = 10_000,
                         ):
     random.shuffle(mad_files)
     mad_files = mad_files[:max_samples]
 
+    lru_cache_size = max(max_samples, 1_000)
+
     return AddBackgroundNoise(
-        mad_files, min_snr_db=min_snr_db, max_snr_db=max_snr_db, p=p, lru_cache_size=max_samples)
+        mad_files, min_snr_db=min_snr_db, max_snr_db=max_snr_db, p=p, lru_cache_size=lru_cache_size)
 
 
 ###############################################################################
@@ -49,9 +51,9 @@ def make_mad_bg_single_class(path_to_mad,
                              min_snr_db: float = 3.0,
                              max_snr_db: float = 30.0,
                              p: float = 0.5,
-                             max_samples: int = 1_000,
+                             max_samples: int = 10_000,
                              ):
-    mad_files = [os.path.join(path_to_mad, f) for f in os.listdir(path_to_mad) if f.endswith(".wav") and mad_class.value[0] in f]
+    mad_files = [os.path.join(path_to_mad, f) for f in os.listdir(path_to_mad) if f.endswith(".wav") and mad_class.value in f]
     return _make_mad_bg_common(mad_files, min_snr_db, max_snr_db, p, max_samples)
 
 
@@ -60,9 +62,9 @@ def make_mad_bg_exclude(path_to_mad: str,
                         min_snr_db: float = 3.0,
                         max_snr_db: float = 30.0,
                         p: float = 0.5,
-                        max_samples: int = 1_000,
+                        max_samples: int = 10_000,
                         ):
-    mad_files = [os.path.join(path_to_mad, f) for f in os.listdir(path_to_mad) if f.endswith(".wav") and class_to_exclude.value[0] not in f]
+    mad_files = [os.path.join(path_to_mad, f) for f in os.listdir(path_to_mad) if f.endswith(".wav") and class_to_exclude.value not in f]
     return _make_mad_bg_common(mad_files, min_snr_db, max_snr_db, p, max_samples)
 
 
@@ -71,7 +73,7 @@ def make_mad_bg_multi_class(path_to_mad: str,
                             min_snr_db: float = 3.0,
                             max_snr_db: float = 30.0,
                             p: float = 0.5,
-                            max_samples: int = 1_000,
+                            max_samples: int = 10_000,
                             ):
     pattern = re.compile(f"(?:{'|'.join(c.value[0] for c in mad_classes)}).+.wav")
     mad_files = [os.path.join(path_to_mad, f) for f in os.listdir(path_to_mad) if re.search(pattern, f)]
