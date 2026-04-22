b288864061b3f61c193aeb1be45144bb63d4c9f2
Ryan Quinn <ryan.quinn@certusinnovations.com>
2026-04-22 14:52:17 -0400

    fix: Add missing exports

--- audioaug/__init__.py
+++ audioaug/__init__.py
@@ -1,3 +1,5 @@
 from .EscBackgroundNoise import create_esc_augment
 from .Infrasound import Infrasound, NoiseLevelType
+from .LocationBasedRir import make_location_rir, RirLocation
 from .MadBackgroundNoise import make_mad_bg_exclude, make_mad_bg_multi_class, make_mad_bg_single_class, MadClass
+from .RealisticBackgroundNoise import make_background_noise_source, BackgroundNoise
